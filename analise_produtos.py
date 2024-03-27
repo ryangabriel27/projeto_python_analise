29a40c6acb956328df79072f859c76edc358a878
ryangabriel27 <ryanjulio56@gmail.com>
2024-03-27 19:46:41 -0300

    aa

--- analise_produtos.py
+++ analise_produtos.py
@@ -8,32 +8,39 @@ with open("empresa.yaml", "r") as file:
 
 # Converta os dados em um DataFrame de vendas
 df_vendas = pd.DataFrame(data["vendas"])
-df_produtos = pd.DataFrame(data["desempenho_produtos"])
+df_produtos = pd.DataFrame(data["desempenho_do_produto"])
 
 # Identifique os produtos mais vendidos
 produtos_mais_vendidos = df_vendas["produto"].head()
-# Agrupar as vendas por produto e calcular a soma da quantidade vendida de cada produto
+
+# Calcular a quantidade vendida e a receita total por produto
+df_vendas["receita"] = df_vendas["quantidade"] * df_vendas["preco_unitario"]
 vendas_por_produto = (
-    df_vendas.groupby("produto")["quantidade"]
-    .sum()
-    .reset_index(name="quantidade_vendida_por_produto")
+    df_vendas.groupby("produto")
+    .agg(quantidade_vendida=("quantidade", "sum"), receita_total=("receita", "sum"))
+    .reset_index()
 )
 
-# Calcular a receita total gerada por cada produto
-df_vendas["receita"] = df_vendas["quantidade"] * df_vendas["preco_unitario"]
-receita_por_produto = (
-    df_vendas.groupby("produto")["receita"]
-    .sum()
-    .reset_index(name="receita_total_por_produto")
-)    
+# Atualizar o DataFrame df_produtos com os valores calculados
+for index, row in df_produtos.iterrows():
+    produto = row["produto"]
+    vendas_produto = vendas_por_produto[vendas_por_produto["produto"] == produto]
+    if not vendas_produto.empty:
+        df_produtos.loc[index, "vendas_totais"] = vendas_produto[
+            "quantidade_vendida"
+        ].values[0]
+        df_produtos.loc[index, "receita_total"] = vendas_produto[
+            "receita_total"
+        ].values[0]
+    else:
+        # Se o produto não tiver vendas, atribuir 0
+        df_produtos.loc[index, "vendas_totais"] = 0
+        df_produtos.loc[index, "receita_total"] = 0
+
 
 # -- GRÁFICO DE BARRA MOSTRANDO A QUANTIDADE DE PRODUTOS VENDIDA
 plt.figure(figsize=(10, 6))
-plt.bar(
-    vendas_por_produto["produto"],
-    vendas_por_produto["quantidade_vendida_por_produto"],
-    color="darkblue",
-)
+plt.bar(df_produtos["produto"], df_produtos["vendas_totais"], color="darkblue")
 plt.title("Quantidade Vendida de Cada Produto")
 plt.xlabel("Produto")
 plt.ylabel("Quantidade Vendida")
@@ -44,11 +51,14 @@ plt.show()
 # -- GRÁFICO DE PIZZA ENTRE AS RECEITAS DOS PRODUTOS
 plt.figure(figsize=(8, 8))
 plt.pie(
-    receita_por_produto["receita_total_por_produto"],
-    labels=receita_por_produto["produto"],
+    df_produtos["receita_total"],
+    labels=df_produtos["produto"],
     autopct="%1.1f%%",
     startangle=140,
 )
-plt.title("Receita dos Produtos")
-plt.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title("Receita Gerada por Produto")
+plt.axis("equal")  # Assegura que o gráfico de pizza seja desenhado como um círculo
 plt.show()
+
+
+# Com esses gráficos concluimos que o Ofurô foi o produto mais vendido com mais de 40 unidades vendidas, e o produto que gerou uma maior receita foi o SPA correspondendo a 47.7%
